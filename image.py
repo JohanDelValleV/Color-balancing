49a45d27d348d9a0bbef17e224bdae90421cce48
Johan Del Valle <43177845+JohanDelValleV@users.noreply.github.com>
2020-02-13 21:16:04 -0600

    Upgraded  UI

--- image.py
+++ image.py
@@ -1,117 +1,151 @@
 import cv2
 import numpy as np
+import tkinter as tk
 from tkinter import *
+from tkinter import ttk
 from tkinter import filedialog
+from tkinter import messagebox
 
 pathIn = ''
 interface = Tk()
 
 
+combo = ttk.Combobox(interface, values=("Gray-world", "Scale-by-max", "Shades-of-gray"),
+                     state="readonly")
+combo.current(0)
+entry = ttk.Entry(interface)
+entry.insert(0, 5.5)
+separador = ttk.Separator(interface, orient=tk.HORIZONTAL)
+
+
 def openFile():
     global pathIn
     imagenPath = filedialog.askopenfilename(
-        title="Seleccionar archivo de video")
+        title="Seleccionar archivo de video", filetypes=[("Image File", '.jpg'), ("Image File", '.png'), ("Image File", '.jpeg')])
     pathIn = imagenPath
+    if pathIn == "":
+        messagebox.showinfo("¡ERROR!", "Debes seleccionar una imagen")
+
     interface.mainloop()
 
 
-def processImageWithMin():
+def grayWorld():
     minimo = []
-    image = cv2.imread(pathIn)
-    cv2.imshow('Original', image)
-    SB = np.sum(image[:, :, 0])
-    SG = np.sum(image[:, :, 1])
-    SR = np.sum(image[:, :, 2])
+    if pathIn == "":
+        messagebox.showinfo("¡ERROR!", "Debes seleccionar una imagen")
+    else:
+        image = cv2.imread(pathIn)
+        cv2.imshow('Original', image)
+        SB = np.sum(image[:, :, 0])
+        SG = np.sum(image[:, :, 1])
+        SR = np.sum(image[:, :, 2])
 
-    print(f'SB:{SB}, SG:{SG}, SR:{SR}')
+        print(f'SB:{SB}, SG:{SG}, SR:{SR}')
 
-    minimo.append(SB)
-    minimo.append(SG)
-    minimo.append(SR)
+        minimo.append(SB)
+        minimo.append(SG)
+        minimo.append(SR)
 
-    Smin = np.amin(minimo)
-    print(f'Min: {Smin}')
-    KB = Smin/SB
-    KG = Smin/SG
-    KR = Smin/SR
+        Smin = np.amin(minimo)
+        print(f'Min: {Smin}')
+        KB = Smin/SB
+        KG = Smin/SG
+        KR = Smin/SR
 
-    print(f'KB:{KB}, KG:{KG}, KR:{KR}')
+        print(f'KB:{KB}, KG:{KG}, KR:{KR}')
 
-    image[:, :, 0] = image[:, :, 0] * KB
-    image[:, :, 1] = image[:, :, 1] * KG
-    image[:, :, 2] = image[:, :, 2] * KR
+        image[:, :, 0] = image[:, :, 0] * KB
+        image[:, :, 1] = image[:, :, 1] * KG
+        image[:, :, 2] = image[:, :, 2] * KR
 
-    cv2.imshow('New image', image)
-    cv2.waitKey()
+        cv2.imshow('New image', image)
 
 
-def processImageWithMax():
+def scaleByMax():
     maximo = []
-    image = cv2.imread(pathIn)
-    cv2.imshow('Original', image)
-    SB = np.amax(image[:, :, 0])
-    SG = np.amax(image[:, :, 1])
-    SR = np.amax(image[:, :, 2])
+    if pathIn == "":
+        messagebox.showinfo("¡ERROR!", "Debes seleccionar una imagen")
+    else:
+        image = cv2.imread(pathIn)
+        cv2.imshow('Original', image)
+        SB = np.amax(image[:, :, 0])
+        SG = np.amax(image[:, :, 1])
+        SR = np.amax(image[:, :, 2])
 
-    print(f'SB:{SB}, SG:{SG}, SR:{SR}')
+        print(f'SB:{SB}, SG:{SG}, SR:{SR}')
 
-    maximo.append(SB)
-    maximo.append(SG)
-    maximo.append(SR)
+        maximo.append(SB)
+        maximo.append(SG)
+        maximo.append(SR)
 
-    Smax = np.amin(maximo)
-    print(f'Max {Smax}')
+        Smax = np.amin(maximo)
+        print(f'Max {Smax}')
 
-    KB = Smax/SB
-    KG = Smax/SG
-    KR = Smax/SR
+        KB = Smax/SB
+        KG = Smax/SG
+        KR = Smax/SR
 
-    print(f'KB:{KB}, KG:{KG}, KR:{KR}')
+        print(f'KB:{KB}, KG:{KG}, KR:{KR}')
 
-    image[:, :, 0] = image[:, :, 0] * KB
-    image[:, :, 1] = image[:, :, 1] * KG
-    image[:, :, 2] = image[:, :, 2] * KR
+        image[:, :, 0] = image[:, :, 0] * KB
+        image[:, :, 1] = image[:, :, 1] * KG
+        image[:, :, 2] = image[:, :, 2] * KR
 
-    cv2.imshow('New image', image)
+        cv2.imshow('New image', image)
 
 
-def processImageWithShades():
+def shadesOfGray():
     minimo = []
-    p = 5.5
-    image = cv2.imread(pathIn)
-    cv2.imshow('Original', image)
-    SB = np.sum(image[:, :, 0]**p)**(1/p)
-    SG = np.sum(image[:, :, 1]**p)**(1/p)
-    SR = np.sum(image[:, :, 2]**p)**(1/p)
+    p = float(entry.get())
+    if pathIn == "":
+        messagebox.showinfo("¡ERROR!", "Debes seleccionar una imagen")
+    else:
+        image = cv2.imread(pathIn)
+        cv2.imshow('Original', image)
+        SB = np.sum(image[:, :, 0]**p)**(1/p)
+        SG = np.sum(image[:, :, 1]**p)**(1/p)
+        SR = np.sum(image[:, :, 2]**p)**(1/p)
 
-    print(f'SB:{SB}, SG:{SG}, SR:{SR}')
+        print(f'SB:{SB}, SG:{SG}, SR:{SR}')
 
-    minimo.append(SB)
-    minimo.append(SG)
-    minimo.append(SR)
+        minimo.append(SB)
+        minimo.append(SG)
+        minimo.append(SR)
 
-    Smin = np.amin(minimo)
-    print(f'Min: {Smin}')
-    KB = Smin/SB
-    KG = Smin/SG
-    KR = Smin/SR
+        Smin = np.amin(minimo)
+        print(f'Min: {Smin}')
+        KB = Smin/SB
+        KG = Smin/SG
+        KR = Smin/SR
 
-    print(f'KB:{KB}, KG:{KG}, KR:{KR}')
+        print(f'KB:{KB}, KG:{KG}, KR:{KR}')
 
-    image[:, :, 0] = image[:, :, 0] * KB
-    image[:, :, 1] = image[:, :, 1] * KG
-    image[:, :, 2] = image[:, :, 2] * KR
+        image[:, :, 0] = image[:, :, 0] * KB
+        image[:, :, 1] = image[:, :, 1] * KG
+        image[:, :, 2] = image[:, :, 2] * KR
 
-    cv2.imshow('New image', image)
-    cv2.waitKey()
+        cv2.imshow('New image', image)
 
 
-Button(interface, text='Seleccionar imagen',
+def checkCombo():
+    if combo.get() == "Gray-world":
+        grayWorld()
+    elif combo.get() == "Scale-by-max":
+        scaleByMax()
+    elif combo.get() == "Shades-of-gray":
+        shadesOfGray()
+    elif combo.get() == "":
+        messagebox.showinfo("¡ERROR!")
+
+
+photo = PhotoImage(file=r"btn.png")
+
+Button(interface, text='Seleccionar imagen', image=photo, compound=LEFT,
        command=openFile).grid(row=1, column=1)
-Button(interface, text='Procesar imagen min',
-       command=processImageWithMin).grid(row=1, column=2)
-Button(interface, text='Procesar imagen max',
-       command=processImageWithMax).grid(row=1, column=3)
-Button(interface, text='Procesar imagen escala de grises',
-       command=processImageWithShades).grid(row=1, column=4)
+Label(interface, text="P").grid(row=1, column=2)
+entry.grid(row=1, column=3)
+separador.grid(row=1, column=4, sticky=tk.W)
+combo.grid(row=1, column=5)
+Button(interface, text='Processar imagen',
+       command=checkCombo).grid(row=2, column=3)
 interface.mainloop()
